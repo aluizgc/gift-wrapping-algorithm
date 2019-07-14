2be49a5155feafa95571dcbc37c1b965ac38ae9f
André Luiz <50808997+aluizgc@users.noreply.github.com>
2019-07-14 03:10:54 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,79 @@
+
+const pontos = []
+const hull = []
+let extremoEsq
+let verticeAtual
+let index
+let proxIndex = -1
+let proxVertice
+
+function setup() {
+  createCanvas(500, 500)
+  let buffer = 20
+  for (let i = 0; i<100;i++){
+  pontos.push(createVector(random(buffer,width-buffer), random(buffer,height-buffer)))
+  }
+  
+  pontos.sort((a,b) => a.x - b.x)
+  extremoEsq = pontos[0]
+  verticeAtual = extremoEsq
+  hull.push(verticeAtual)
+  proxVertice = pontos[1]
+  index = 2
+}
+
+function draw() {
+  background(0)
+  
+  stroke(255)
+  strokeWeight(8)
+  for (let p of pontos){
+    point(p.x,p.y)
+  }
+  
+  stroke(0,0,255)
+  fill(0,0,255,50)
+  beginShape()
+  for (let p of hull){
+    vertex(p.x,p.y)
+  }
+  endShape(CLOSE)
+  
+  stroke(0,255,0)
+  strokeWeight(20)
+  point(extremoEsq.x,extremoEsq.y)
+  
+  stroke(200,0,255)
+  strokeWeight(20)
+  point(verticeAtual.x,verticeAtual.y)
+  
+  stroke(0,255,0)
+  strokeWeight(2)
+  line(verticeAtual.x, verticeAtual.y, proxVertice.x, proxVertice.y)
+  
+  let checando = pontos[index]
+  stroke(255)
+  line(verticeAtual.x, verticeAtual.y, checando.x, checando.y)
+  
+  const a = p5.Vector.sub(proxVertice, verticeAtual)
+  const b = p5.Vector.sub(checando, verticeAtual)
+  const vetorial = a.cross(b)
+  if (vetorial.z < 0){
+    proxVertice = checando
+    proxIndex = index
+  }
+  index = index + 1
+  if (index == pontos.length){
+    if (proxVertice == extremoEsq){
+      console.log('Pronto')
+      noLoop()
+    }else{
+      hull.push(proxVertice)
+      verticeAtual = proxVertice
+      index = 0
+      //pontos.splice(proxIndex,1)
+      proxVertice = extremoEsq
+
+    }
+  }
+}
